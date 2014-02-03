2148cea3c3d0312c735f5ab87073add31252f38d
Paul Koerbitz <paul.koerbitz@theppro.com>
2014-02-03 20:55:23 +0100

    Add changes of Worksheet02.hs

--- src/HaskellWorkshop/Worksheet02.hs
+++ src/HaskellWorkshop/Worksheet02.hs
@@ -1,33 +1,81 @@
 module HaskellWorkshop.Worksheet02
        ( Tree(..)
-       , treeSize
+       , numOfElements
        , treeSize'
        , treeSize''
        , treeMax
        , treeMax'
        , treeMap
        , treeFold
-       , ) where
+       , myList
+       , length'
+       , length''
+       ) where
 
 --------------------------------------------------------------------------------
 -- Define a bivariate tree. Tip: A tree is much like a list, except that it
 -- has two branches
 --------------------------------------------------------------------------------
-data Tree a = Nil
+data List a = Nil
+            | Cons a (List a)
+
+myList :: List Int
+myList = Cons 1 (Cons 2 (Cons 3 Nil))
+
+
+length' :: List a -> Int
+length' Nil         = 0
+length' (Cons _ xs) = 1 + length' xs
+
+-- (1 + (1 + (1 + (1 + ........ (1 + 0)))))
+
+length_tailrec :: List a -> Int
+length_tailrec xs = len 0 xs
+  where
+    len acc Nil         = acc
+    len acc (Cons _ ys) = len (acc+1) ys
+
+
+length'' :: [a] -> Int
+length'' []     = 0
+length'' (_:xs) = 1 + length'' xs
+
+data Tree a = Tip
+            | Node (Tree a) a (Tree a)
+
+myTree2 :: Tree Int
+myTree2 = Node Tip 3 Tip
 
 
 --------------------------------------------------------------------------------
 -- Write a function that computes the size of the tree
 --------------------------------------------------------------------------------
-treeSize :: Tree a -> Int
-treeSize = undefined
+numOfElements :: Tree a -> Int
+numOfElements Tip          = 0
+numOfElements (Node l x r) = numOfElements l + 1 + numOfElements r
+
+myTree3 :: Tree Int
+myTree3 = Node (Node Tip 3 Tip) 4 (Node Tip 5 Tip)
+
+--
+--              4
+--             / \
+--            3   5
+--
+
+treeHeight :: Tree a -> Int
+treeHeight Tip          = 0
+treeHeight (Node l x r) = 1 + max leftHeight rightHeight
+  where
+    leftHeight  = treeHeight l
+    rightHeight = treeHeight r
 
 
 --------------------------------------------------------------------------------
 -- Can you rewrite this function to be tail recursive?
 --------------------------------------------------------------------------------
-treeSize' :: Tree a -> Int
-treeSize' = undefined
+numOfElements' :: Tree a -> Int
+numOfElements' = undefined
 
 
 --------------------------------------------------------------------------------
